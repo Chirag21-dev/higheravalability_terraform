b705b6f0b7af065f5d0831398366c8844c306f0c
Chirag21-dev <your_email@example.com>
2026-01-01 17:58:32 +0530

    Updated the alb with alb listener block.

--- alb.tf
+++ alb.tf
@@ -30,3 +30,14 @@ resource "aws_lb_target_group" "app_tg" {
     Name = "app-target-group"
   }
 }
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
